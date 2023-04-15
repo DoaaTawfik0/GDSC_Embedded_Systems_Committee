2115c5a81560a210e3ad306811267a41ce07d195
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2023-04-15 18:11:40 +0200

    main.c

--- GDSC_AVR_Tasks/Week1_Task/Task2/main.c
+++ GDSC_AVR_Tasks/Week1_Task/Task2/main.c
@@ -17,40 +17,30 @@
 #include <util/delay.h>
 int main (void)
 {
-    SET_BIT(DDRA , 0);
-    SET_BIT(DDRA , 1);
-    SET_BIT(DDRA , 2);
-    SET_BIT(DDRA , 3);
-    SET_BIT(DDRA , 4);
-    SET_BIT(DDRA , 5);
+	u8 i;
+
+    //Setting bits from 0 to 5 direction as OUTPUT
+    for(i = 0 ; i < 6 ; i++)
+    {
+    	SET_BIT(DDRA , i);
+    }
 	while(1)
 	{
 		//Setting bits from 0 to 5
-		SET_BIT(PORTA , 0);
-		_delay_ms(100);
-		SET_BIT(PORTA , 1);
-		_delay_ms(100);
-		SET_BIT(PORTA , 2);
-		_delay_ms(100);
-		SET_BIT(PORTA , 3);
-		_delay_ms(100);
-		SET_BIT(PORTA , 4);
-		_delay_ms(100);
-		SET_BIT(PORTA , 5);
-		_delay_ms(100);
+		for(i = 0 ; i < 6 ; i++)
+		{
+			SET_BIT(PORTA , i);
+			_delay_ms(100);
+
+		}
 		//Clearing bits from 0 to 5
-		CLEAR_BIT(PORTA , 0);
-		_delay_ms(100);
-		CLEAR_BIT(PORTA , 1);
-		_delay_ms(100);
-		CLEAR_BIT(PORTA , 2);
-		_delay_ms(100);
-		CLEAR_BIT(PORTA , 3);
-		_delay_ms(100);
-		CLEAR_BIT(PORTA , 4);
-		_delay_ms(100);
-		CLEAR_BIT(PORTA , 5);
-		_delay_ms(100);
+		for(i = 0 ; i < 6 ; i++)
+		{
+			CLEAR_BIT(PORTA , i);
+			_delay_ms(100);
+
+		}
+
 	}
 
 
